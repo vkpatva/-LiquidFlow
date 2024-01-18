bb6034d418db80d008467abff5bc37bd487e4f19
vkpatva <19it117@charusat.edu.in>
2024-01-18 08:18:01 +0530

    SC: Deployed new contract and subgraph

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const AccountFactory = '0x0C5293daf3C0bec8CE3683266C59E6ae4E9558E2'
-export const LiquidityFlow = '0x396a361d90bda0edec94a198972d2286434cdfd7'
-export const LiquidityFlowGraphQL = 'https://api.studio.thegraph.com/query/51753/lfgho/v0.0.1'
+export const LiquidityFlow = '0xb9a7816fE5d986a261227A23A11D98C886a8627f'
+export const LiquidityFlowGraphQL = 'https://api.studio.thegraph.com/query/51753/lfgho/v0.0.2'
