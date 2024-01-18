1d12b740896a914617a81f63f4bcce90416d5758
vkpatva <19it117@charusat.edu.in>
2024-01-18 23:24:50 +0530

    FE: Trade card changed

--- frontend/src/components/TradeCard.component.tsx
+++ frontend/src/components/TradeCard.component.tsx
@@ -1,12 +1,13 @@
 import {
   shortenAddress,
   useAddress,
-  useSDK,
+  useContract,
+  useContractRead,
   useSigner,
 } from "@thirdweb-dev/react";
 import { useEffect, useState } from "react";
 import { requestFinance } from "../lib/SmartContract";
-
+import { LiquidityFlow } from "../utils/constants";
 type TradeCardProps = {
   tradeDescription: string;
   payerAddress: string;
@@ -27,16 +28,32 @@ export const TradeCard = ({
   const address = useAddress();
   const signer = useSigner();
   const [isReceiver, setIsReceiver] = useState(false);
+  const { contract } = useContract(LiquidityFlow);
+  const { data, isLoading } = useContractRead(contract, "getTradeDetails", [
+    id,
+  ]);
+  const [returnPercentage, setReturnPercentage] = useState(100);
+  const [financed, setIsFinanced] = useState(false);
+  const [isInvested, setIsInvested] = useState(false);
   useEffect(() => {
     console.log("Trade Card", { address, receiverAddress });
     setIsReceiver(address?.toLowerCase() == receiverAddress.toLowerCase());
   }, []);
-  const returnPercentage = 0;
-  const financed = false;
-  const isInvested = false;
+  useEffect(() => {
+    if (data) {
+      if (parseInt(data.financeAmount)) {
+        setIsFinanced(true);
+      }
+      setIsInvested(data.isFinanced);
+      setReturnPercentage(
+        ((parseInt(data.amount) - parseInt(data.financeAmount)) * 100) /
+          parseInt(data.amount)
+      );
+    }
+  }, [data]);
 
   return (
-    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl border border-gray-300 hover:border-indigo-500 transition-colors duration-300  min-h-[240px] bg-gradient-to-r from-blue-100 to-indigo-100">
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl border border-gray-300 hover:border-indigo-500 transition-colors duration-300  min-h-[280px] bg-gradient-to-r from-blue-100 to-indigo-100">
       <div className="md:flex  p-6 h-full">
         <div className="flex-1 min-w-0">
           <p className="text-md font-semibold text-indigo-700 mb-4">
@@ -50,7 +67,7 @@ export const TradeCard = ({
           </p>
           <p className="text-gray-500">Amount: {amount}</p>
           <p className="text-gray-500">Days: {days}</p>
-          {returnPercentage ? (
+          {returnPercentage != 100 ? (
             <p className="text-green-700 mt-2">Return: {returnPercentage}%</p>
           ) : (
             ""
@@ -59,7 +76,14 @@ export const TradeCard = ({
             <button
               className="mt-4 bg-indigo-500 text-white py-2 px-4 rounded hover:bg-indigo-600"
               onClick={() => {
-                requestFinance(signer, id, Math.floor(0.95 * parseInt(amount)));
+                requestFinance(
+                  signer,
+                  id,
+                  Math.floor(
+                    ((Math.floor(Math.random() * 7) + 1) / 10) *
+                      parseInt(amount)
+                  )
+                );
               }}
             >
               Request Finance
